d0a4eac82bdec9e2ee579921faf59f5898674b2c
swdecker <swdecker@gmail.com>
2019-12-03 13:15:37 -0600

    began adding login functions

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   scope '/api' do
     resources :days
     resources :tasks
-    resources :users
+    resources :users, only: [:create, :show, :index]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

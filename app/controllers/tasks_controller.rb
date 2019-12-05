d76f2315bf1d9df9f224c4e18b570e23e3169c68
swdecker <swdecker@gmail.com>
2019-12-05 13:21:03 -0600

    updated seed data, serialized tasks and associated data,added tasks#show route for a users tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,30 @@
 class TasksController < ApplicationController
 
   def index
-    tasks = Task.all.select{ |task| task.user_id === 1 }
-    categories = Category.all.select{ |category| category.user_id === 1 }
+    tasks = Task.all
+    categories = Category.all
     render json: {
       tasks: tasks,
       categories: categories
     }
+  end
+  def show
+    user_id = params[:id]
+    @user = User.find(params[:id])
+    if logged_in? && @user == current_user
+      tasks = Task.where(user_id: user_id)
+      categories = Category.where(user_id: user_id)
+      locations = Location.where(user_id: user_id)
+      options = {
+        include: [:category, :location]
+      }
+      render json: {
+        tasks:TaskSerializer.new(tasks, options),
+        categories: categories,
+        locations: locations
+      }
+    end
+
   end
 
   def create
